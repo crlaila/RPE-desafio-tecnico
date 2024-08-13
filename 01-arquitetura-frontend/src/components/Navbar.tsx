0782f03ca6a336ed4bbcd5e1b2de8c6b24a8d5cf
Laila Silva <laila.cristine@outlook.com>
2024-08-13 19:24:09 -0300

    reestruturação das pastas para exemplicar a arquitetura pensada no projeto

---
+++ 01-arquitetura-frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #191919;
+  color: white;
+  padding: 20px;
+`;
+
+export function Navbar() {
+  return <Container>Seja bem-vindo(a) a nossa loja!</Container>;
+}
+
+export default Navbar;
