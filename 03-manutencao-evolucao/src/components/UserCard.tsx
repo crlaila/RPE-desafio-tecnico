06939f859704d71706fa0ed019a77413e1e975ef
Laila Silva <laila.cristine@outlook.com>
2024-08-14 18:32:34 -0300

    componente para exibicao de um unico usuario

---
+++ 03-manutencao-evolucao/src/components/UserCard.tsx
@@ -0,0 +1,21 @@
+// components/UserCard.tsx
+import React from "react";
+import { User } from "../types/user";
+
+const UserCard: React.FC<User> = ({ name, email, avatar }) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-4 flex items-center space-x-4">
+      <img
+        className="w-16 h-16 rounded-full object-cover"
+        src={avatar}
+        alt={name}
+      />
+      <div>
+        <h2 className="text-lg font-bold">{name}</h2>
+        <p className="text-gray-500">{email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
